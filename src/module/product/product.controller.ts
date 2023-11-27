dbca580fbaedf4fd27e67a808017850b34745525
hothanhvawn <vanho@playgroundvina.com>
2023-11-27 16:44:26 +0700

    update redis

--- src/module/product/product.controller.ts
+++ src/module/product/product.controller.ts
@@ -6,24 +6,31 @@ import {
   Patch,
   Param,
   UseInterceptors,
+  Inject,
 } from "@nestjs/common";
 import { ProductService } from "./product.service";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
 import { Types } from "mongoose";
-import { CacheInterceptor } from "@nestjs/cache-manager";
+import { CACHE_MANAGER, CacheInterceptor } from "@nestjs/cache-manager";
+import { Cache } from "cache-manager";
 
 @Controller("product")
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(
+    private readonly productService: ProductService,
+
+    @Inject(CACHE_MANAGER)
+    private cacheManage: Cache,
+  ) {}
 
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
   }
 
-  @UseInterceptors(CacheInterceptor)
-  @Get()
+  // @UseInterceptors(CacheInterceptor)
+  @Get("no")
   findAll() {
     return this.productService.findAll();
   }
@@ -31,5 +38,20 @@ export class ProductController {
   @Patch(":id")
   async update(@Param("id") id: Types.ObjectId, @Body() dto: UpdateProductDto) {
     return await this.productService.update(id, dto);
+  }
+
+  @Get()
+  async getCache() {
+    const val = await this.cacheManage.get("list");
+    if (val) {
+      return {
+        data: val,
+        message: "this is loader from redis cache",
+      };
+    } else {
+      const data = this.productService.findAll();
+      await this.cacheManage.set("list", data, 1000);
+      return data;
+    }
   }
 }
